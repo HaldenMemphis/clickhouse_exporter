e7e80476efca2e96d5334604df30b5703f49cf8b
刘一帆 <yfliu5969@fiberhome.com>
2022-03-28 13:55:34 +0800

    采集每个表的行数，原始大小，压缩大小，压缩率数据

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -22,6 +22,7 @@ const (
 // Exporter collects clickhouse stats from the given URI and exports them using
 // the prometheus metrics package.
 type Exporter struct {
+	customizeURI    string
 	metricsURI      string
 	asyncMetricsURI string
 	eventsURI       string
@@ -37,6 +38,11 @@ type Exporter struct {
 // NewExporter returns an initialized Exporter.
 func NewExporter(uri url.URL, insecure bool, user, password string) *Exporter {
 	q := uri.Query()
+
+	customizeURI := uri
+	q.Set("query", "select table, sum(data_compressed_bytes) as data_compressed_bytes, sum(bytes) as size, sum(data_uncompressed_bytes) as data_uncompressed_bytes,round((data_compressed_bytes / data_uncompressed_bytes) * 100,2) as compress_rate from system.parts where active and database = 'ods' and table in('ChannelDiagnoseInfoEvent_s120','ChannelDiagnoseInfoEvent_channelInfos_s120','device_heartbeat_record',  'device_service_record', 'device_up_down', 'WifiStatusEvent_s120', 'gatewayDiagnoseInfoEvent_s120', 'speedTestDiagRsp_s120','WifiStatusEvent_s120', 'WifiStatusEvent_wifiStatusList_s120') group by table")
+	customizeURI.RawQuery = q.Encode()
+
 	metricsURI := uri
 	q.Set("query", "select metric, value from system.metrics")
 	metricsURI.RawQuery = q.Encode()
@@ -54,6 +60,8 @@ func NewExporter(uri url.URL, insecure bool, user, password string) *Exporter {
 	partsURI.RawQuery = q.Encode()
 
 	return &Exporter{
+
+		customizeURI:    customizeURI.String(),
 		metricsURI:      metricsURI.String(),
 		asyncMetricsURI: asyncMetricsURI.String(),
 		eventsURI:       eventsURI.String(),
@@ -97,6 +105,62 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (e *Exporter) collect(ch chan<- prometheus.Metric) error {
+	rows, err := e.parseCustomizeResponse(e.customizeURI)
+	if err != nil {
+		return fmt.Errorf("Error scraping clickhouse url %v: %v", e.customizeURI, err)
+	}
+	for _, x := range rows {
+		newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      metricName(x.key + "_rows"),
+			Help:      "Number of " + x.key + "_rows currently processed",
+		}, []string{}).WithLabelValues()
+		newMetric.Set(x.rows)
+		newMetric.Collect(ch)
+	}
+
+	originalSizes, err := e.parseCustomizeResponse(e.customizeURI)
+	if err != nil {
+		return fmt.Errorf("Error scraping clickhouse url %v: %v", e.customizeURI, err)
+	}
+	for _, x := range originalSizes {
+		newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      metricName(x.key + "_original_size_bytes"),
+			Help:      "Number of " + x.key + "originalSizes currently processed",
+		}, []string{}).WithLabelValues()
+		newMetric.Set(x.originalSize)
+		newMetric.Collect(ch)
+	}
+
+	compressedSizes, err := e.parseCustomizeResponse(e.customizeURI)
+	if err != nil {
+		return fmt.Errorf("Error scraping clickhouse url %v: %v", e.customizeURI, err)
+	}
+	for _, x := range compressedSizes {
+		newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      metricName(x.key + "_compressed_size_bytes"),
+			Help:      "Number of " + x.key + "_compressed_size currently processed",
+		}, []string{}).WithLabelValues()
+		newMetric.Set(x.compressedSize)
+		newMetric.Collect(ch)
+	}
+
+	compressionRate, err := e.parseCustomizeResponse(e.customizeURI)
+	if err != nil {
+		return fmt.Errorf("Error scraping clickhouse url %v: %v", e.customizeURI, err)
+	}
+	for _, x := range compressionRate {
+		newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      metricName(x.key + "_compression_rate"),
+			Help:      "Number of " + x.key + "_compression_rate currently processed",
+		}, []string{}).WithLabelValues()
+		newMetric.Set(x.compressionRate)
+		newMetric.Collect(ch)
+	}
+
 	metrics, err := e.parseKeyValueResponse(e.metricsURI)
 	if err != nil {
 		return fmt.Errorf("Error scraping clickhouse url %v: %v", e.metricsURI, err)
@@ -206,6 +270,14 @@ type lineResult struct {
 	value float64
 }
 
+type customizeResult struct {
+	key             string
+	rows            float64
+	originalSize    float64
+	compressedSize  float64
+	compressionRate float64
+}
+
 func parseNumber(s string) (float64, error) {
 	v, err := strconv.ParseFloat(s, 64)
 	if err != nil {
@@ -213,6 +285,38 @@ func parseNumber(s string) (float64, error) {
 	}
 
 	return v, nil
+}
+
+func (e *Exporter) parseCustomizeResponse(uri string) ([]customizeResult, error) {
+	data, err := e.handleResponse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parsing results
+	lines := strings.Split(string(data), "\n")
+	var results []customizeResult = make([]customizeResult, 0)
+
+	for i, line := range lines {
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+		if len(parts) != 5 {
+			return nil, fmt.Errorf("parseCustomizeResponse: unexpected %d line: %s", i, line)
+		}
+		k := strings.TrimSpace(parts[0])
+		v1, err := parseNumber(strings.TrimSpace(parts[1]))
+		v2, err := parseNumber(strings.TrimSpace(parts[2]))
+		v3, err := parseNumber(strings.TrimSpace(parts[3]))
+		v4, err := parseNumber(strings.TrimSpace(parts[4]))
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, customizeResult{k, v1, v2, v3, v4})
+
+	}
+	return results, nil
 }
 
 func (e *Exporter) parseKeyValueResponse(uri string) ([]lineResult, error) {
